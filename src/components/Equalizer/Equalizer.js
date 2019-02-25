aafde7cc292908a313266f60e68fa575c8eb30fe
RSamuel Author Services <you@example.comrejinisamuel@gmail.com>
2019-02-25 10:17:41 -0500

    css tweaks for scrolling and width

--- src/components/Equalizer/Equalizer.js
+++ src/components/Equalizer/Equalizer.js
@@ -4,20 +4,24 @@ import Button from "@material-ui/core/Button";
 const Equalizer = ({ selectedTraits, closeEq }) => {
   return (
     <section>
-      <div>
-        <ul>
-          {selectedTraits.map((element, i) => {
-            return (
-              <li
-                className="w-95 bg-purple o-80 br3 pa2 ma3 shadow-5 "
-                key={selectedTraits[i]}
-              >
-                "{element[0]}" played at "{element[1]}" in "{element[2]}"
-              </li>
-            );
-          })}
-        </ul>
-
+      <div className="appscroll">
+        <h2>Character Equalizer Results</h2>
+        <div id="listDiv" className="scroller">
+          <ul className="scroller">
+            {selectedTraits.map((element, i) => {
+              return (
+                <li
+                  className="item w-95 bg-purple o-80 br3 pa2 ma3 shadow-5 "
+                  key={selectedTraits[i]}
+                >
+                  <div className="item">
+                    "{element[0]}" played at "{element[1]}" in "{element[2]}"
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
         <Button variant="contained" color="primary" onClick={closeEq}>
           RETURN
         </Button>
